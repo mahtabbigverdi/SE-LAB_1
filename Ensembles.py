bf0775402cb3909f80aab6efdb6a0b481900d283
mahtabbigverdi <bigverdi.1377@gmail.com>
2021-10-18 12:39:01 +0330

    add random forest

---
+++ Ensembles.py
@@ -0,0 +1,23 @@
+from sklearn import ensemble
+from sklearn.metrics import accuracy_score
+
+
+class EnsembleAndDTClassifiers():
+    def __init__(self, x_train, y_train, x_test, y_test):
+        self.x_train = x_train
+        self.x_test = x_test
+        self.y_train = y_train
+        self.y_test = y_test
+
+    def random_forest(self, n_estimators = 100 ):
+        model = ensemble.RandomForestClassifie(n_estimators = n_estimators)
+        model.fit(self.x_train, self.y_train)
+        pred = model.predict(self.x_test)
+        return accuracy_score(self.y_test, pred)
+
+
+
+
+
+
+
