4ce75da585ff57923a5193e05ce27b8575461591
mahtabbigverdi <bigverdi.1377@gmail.com>
2021-10-18 13:34:12 +0330

    add load data

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from LinearModel import *
 from SVM import *
 from Ensembles import *
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+
+def load_data():
+    data = load_breast_cancer()
+    return train_test_split(data.data, data.target, test_size=0.33, random_state=42)
+
 
 if __name__ == '__main__':
-    x_train, y_train, x_test, y_test = None, None, None, None
+
+    x_train, x_test, y_train, y_test = load_data()
     
-    print('Welcome to BreastCancerClassifier application! Please choose your desired model category and enter a number:'
+    print('Welcome to BreastCancerClassifier application! \n Please choose your desired model category and enter a number:'
           '\n1-LinearModel'
           '\n2-EnsembleModels and DecisionTrees'
           '\n3-SVM')
@@ -48,6 +56,8 @@ if __name__ == '__main__':
         print('input kernel: ')
         kernel = input()
         test_acc = clf.svm_classifier(kernel=kernel)
+
+    print('Test accuracy is ', str(test_acc))
 
 
 
