02158a89e4e6d66538a963c38d81f9ef59949d49
arghavan99 <55342129rez>
2021-10-18 14:17:07 +0330

    refactor

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from LinearModel import *
-from SVM import *
-from Ensembles import *
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
+
+from Ensembles import *
+from LinearModel import *
+from SVM import *
+
 
 def load_data():
     data = load_breast_cancer()
@@ -12,22 +14,23 @@ def load_data():
 if __name__ == '__main__':
 
     x_train, x_test, y_train, y_test = load_data()
-    
-    print('Welcome to BreastCancerClassifier application! \n Please choose your desired model category and enter a number:'
-          '\n1-LinearModel'
-          '\n2-EnsembleModels and DecisionTrees'
-          '\n3-SVM')
+
+    print('Welcome to BreastCancerClassifier application! '
+          '\nPlease choose your desired model category and enter a number:'
+          '\n1. LinearModel'
+          '\n2. EnsembleModels and DecisionTrees'
+          '\n3. SVM')
     cat = int(input())
 
     if cat == 1:
         clf = LinearClassifiers(x_train, y_train, x_test, y_test)
-        print('choose among available linear models below:')
-        print('1.Ridge Classifier')
-        print('2. Logistic Regression')
+        print('choose among available linear models below:'
+              '\n1. Ridge Classifier'
+              '\n2. Logistic Regression')
         num = int(input())
         if num == 1:
             print('input alpha:')
-            alpha = int(input())
+            alpha = float(input())
             test_acc = clf.ridge_classifier(alpha)
         elif num == 2:
             print('input penalty (l1 or l2 ?):')
@@ -36,19 +39,19 @@ if __name__ == '__main__':
 
     if cat == 2:
         clf = EnsembleAndDTClassifiers(x_train, y_train, x_test, y_test)
-        print('choose among available ensemble models below:')
-        print('1. Random Forest')
-        print('2. AdaBoost')
-        print('3. Decision Tree')
+        print('choose among available ensemble models below:'
+              '\n1. Random Forest'
+              '\n2. AdaBoost'
+              '\n3. Decision Tree')
         num = int(input())
         if num == 1:
             print('input number of estimators:')
             n_estimators = int(input())
-            test_acc = clf.random_forest(n_estimators = n_estimators)
+            test_acc = clf.random_forest(n_estimators=n_estimators)
         if num == 2:
             print('input number of estimators:')
             n_estimators = int(input())
-            test_acc = clf.adaboost(n_estimators = n_estimators)
+            test_acc = clf.adaboost(n_estimators=n_estimators)
         if num == 3:
             tets_acc = clf.decision_tree()
     if cat == 3:
@@ -58,6 +61,3 @@ if __name__ == '__main__':
         test_acc = clf.svm_classifier(kernel=kernel)
 
     print('Test accuracy is ', str(test_acc))
-
-
-
