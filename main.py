387264d4e04bb3eed51bd6344e0c0af19bc53007
arghavan99 <55342129rez>
2021-10-18 11:41:48 +0330

    initial command line interface

---
+++ main.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    print('Welcome to BreastCancerClassifier application! Please choose your desired model category and enter a number:'
+          '\n1-LinearModel'
+          '\n2-EnsembleModels and DecisionTrees'
+          '\n3-SVM')
+    cat = int(input())
